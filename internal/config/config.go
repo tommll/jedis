6d0e7a42a27a077824d34be50fd96dadb2d65a1f
Tung <tunglee0305@gmail.com>
2025-06-27 16:20:02 +0700

    feat: add server config

---
+++ internal/config/config.go
@@ -0,0 +1,14 @@
+package config
+
+var Host = "0.0.0.0"
+var Port = 8080
+var MaxConnections = 20000
+var KeyNumberLimit = 5000000
+
+const (
+	EvictFirst int = 0
+	LRU            = 1
+	LFU            = 2
+)
+
+var EvictStrategy = EvictFirst
